3ee8238081a39e12369389b5299cdd1fbc7d7c40
rana <ranamaripillai@gmail.com>
2022-08-18 19:13:00 +0530

    fetch API made for Qualification,personalDetails

--- src/main/java/com/visitingfaculty/dto/UserPersonalDetailsDTO.java
+++ src/main/java/com/visitingfaculty/dto/UserPersonalDetailsDTO.java
@@ -1,53 +1,47 @@
 package com.visitingfaculty.dto;
 
+import java.util.Arrays;
+
 import com.visitingfaculty.model.user_personal_details.UserAddress;
 import com.visitingfaculty.model.user_personal_details.UserContact;
 import com.visitingfaculty.model.user_personal_details.UserInfo;
 
 public class UserPersonalDetailsDTO {
 
-    private UserAddress user_address;
+    private UserAddress user_address[];
     private UserContact user_contact;
     private UserInfo user_info;
-
+    
+    @Override
+    public String toString() {
+        return "UserPersonalDetailsDTO [user_address=" + Arrays.toString(user_address) + ", user_contact="
+                + user_contact + ", user_info=" + user_info + "]";
+    }
     public UserPersonalDetailsDTO() {
     }
-
-    public UserPersonalDetailsDTO(UserAddress user_address, UserContact user_contact, UserInfo user_info) {
+    public UserPersonalDetailsDTO(UserAddress[] user_address, UserContact user_contact, UserInfo user_info) {
         this.user_address = user_address;
         this.user_contact = user_contact;
         this.user_info = user_info;
     }
-
-    public UserAddress getUser_address() {
+    public UserAddress[] getUser_address() {
         return user_address;
     }
-
-    public void setUser_address(UserAddress user_address) {
+    public void setUser_address(UserAddress[] user_address) {
         this.user_address = user_address;
     }
-
     public UserContact getUser_contact() {
         return user_contact;
     }
-
     public void setUser_contact(UserContact user_contact) {
         this.user_contact = user_contact;
     }
-
     public UserInfo getUser_info() {
         return user_info;
     }
-
     public void setUser_info(UserInfo user_info) {
         this.user_info = user_info;
     }
 
-    @Override
-    public String toString() {
-        return "FacultyPersonalDetailsDTO [user_address=" + user_address + ", user_contact=" + user_contact
-                + ", user_info=" + user_info + "]";
-    }
-
-  
+    
 }
