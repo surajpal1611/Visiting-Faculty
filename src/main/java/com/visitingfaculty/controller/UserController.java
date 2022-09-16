55c9980b21314236a8d303bc9a4741d71f7a2ceb
Arun Babu <arunbabu@gmail.com>
2022-09-16 10:40:33 +0530

    changes

--- src/main/java/com/visitingfaculty/controller/UserController.java
+++ src/main/java/com/visitingfaculty/controller/UserController.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.visitingfaculty.dao.UserDaoInterface;
 import com.visitingfaculty.model.User;
 
 @Controller
+@RequestMapping("/vfApp")
 public class UserController {
 
 
